32543f73e0f643125c4a7de24cc05688cb9975e3
Chris Sim <imchrissim@gmail.com>
2014-06-02 04:07:08 -0700

    card assets added, create_deck() initial layout added with navs

--- app/views.py
+++ app/views.py
@@ -2,13 +2,15 @@ from flask import Blueprint, render_template, request, flash, url_for, redirect,
 from app.decorators import login_required
 from app.lib.credential import credential
 from app import app
-import sys
+import sys, os, jinja2, json
+from settings import APP_STATIC
 
 
 ###############################################################################
 # Instantiation
 ###############################################################################
 main = Blueprint('app', __name__, template_folder='templates', static_folder='static')
+app.jinja_env.add_extension('jinja2.ext.do')
 
 
 ###############################################################################
@@ -17,6 +19,8 @@ main = Blueprint('app', __name__, template_folder='templates', static_folder='st
 @main.route('/', methods=['GET', 'POST'])
 def front():
     if request.method == 'GET':
+        if session.get('username'):
+            return redirect(url_for('.menu'))
         return render_template('front.html')
     else:
         success, next_view = credential._validate_credential(request.form)
@@ -24,19 +28,19 @@ def front():
         # TODO: rate limit to 3 tries, then lock for 1hr, redirect to forgot password page
 
 
-@main.route('/menu/')
+@main.route('/menu')
 def menu():
     if session.get('username'):
-        return render_template('menu.html', session=1) 
+        return render_template('menu.html', session=1)
     return render_template('menu.html', session=0)
 
 
-@main.route('/deck/count/')
+@main.route('/deck/count')
 def count_deck():
     return render_template('count_deck.html')
 
 
-@main.route('/deck/')
+@main.route('/deck')
 def choose_deck():
     if session.get('username'):
         # TODO: check if the user has any deck saved, if so, display a thumbnail for each deck
@@ -45,9 +49,16 @@ def choose_deck():
         return render_template('choose_deck.html', decks=[])
 
 
-@main.route('/deck/create/')
+@main.route('/deck/create')
 def create_deck():
-    return render_template('create_deck.html')
+    active = request.args.get('active')
+    view = request.args.get('view')
+    category = request.args.get('category')
+
+    with open(os.path.join(APP_STATIC, 'data/cards.json')) as f:
+        cards = f.read()
+    data = json.loads(cards)
+    return render_template('create_deck.html', active=active, view=view, category=category, data=data['cards'])
 
 
 
@@ -55,6 +66,8 @@ def create_deck():
 @main.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'GET':
+        if session.get('username'):
+            return redirect(url_for('.menu'))
         return render_template('signup.html')
     else:
         success = credential._create_credential(request.form)
